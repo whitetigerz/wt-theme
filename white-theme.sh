3c207bacdb7db989a04a162afb0a78976fb80a78
whitetigerz <50932142+whitetigerz@users.noreply.github.com>
2019-09-01 03:10:47 +0530

    Delete white-theme.sh

--- white-theme.sh
+++
@@ -1,8 +0,0 @@
-m -r -f ~/.bashrc
-echo "NAME=$1" >> ~/.bashrc
-echo "export PS1='
-\[\033[0;31m\]┌─[\[\033[1;34m\]\$NAME\[\033[1;33m\]@\[\033[1;36m\]termux\[\033[0;31m\]]─[\[\033[0;32m\]\w\[\033[0;31m>
-\[\033[0;31m\]└──╼ \[\033[1;33m\]#\[\033[0m\] '" >> ~/.bashrc
-echo "export LS_OPTIONS='--color=auto'" >> ~/.bashrc
-echo "alias ls='ls --color=auto'" >> ~/.bashrc
-echo "alias grep='grep --color=auto'" >> ~/.bashrc
